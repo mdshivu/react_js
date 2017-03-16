0c260029fed2de8933e6a3acded728061d978775
shivu <mdshivu@gmail.com>
2017-03-16 20:38:45 +0530

    second file

---
+++ second.php
@@ -0,0 +1,10 @@
+<?php echo 'second file
+
+cho "# react_js" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mdshivu/react_js.git
+git push -u origin master
+
+'; ?>
